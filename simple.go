9a3bd4d476e1bb2d7c46de862b141c73b7ce4b8c
Danilo Cianfrone <danilo.cianfrone@geouniq.com>
2017-11-12 12:05:30 +0100

    improved insertion

--- simple.go
+++ simple.go
@@ -9,9 +9,27 @@ type SimpleTrie struct {
 	key  string
 	data interface{}
 	sons []SimpleTrie
+	word bool
 }
 
-func (n SimpleTrie) Insert(key string, data interface{}) (Tree, bool) { return n.insert(key, data) }
+func (n SimpleTrie) Insert(key string, data interface{}) (Tree, bool) {
+	if n.isUninit() {
+		return SimpleTrie{key: key, data: data, word: true}, true
+	}
+	if n.isWildcard() {
+		for i, s := range n.sons {
+			p, k1, k2 := lcs(s.key, key)
+			if p != "" { // At least one common prefix on direct sons
+				ns, ok := s.blindInsert(p, k1, k2, data) // Blind insert on common son
+				n.sons[i] = ns
+				return n, ok
+			}
+		}
+		return SimpleTrie{sons: append(n.sons, SimpleTrie{key: key, data: data, word: true})}, true
+	}
+	prefix, k1, k2 := lcs(n.key, key)
+	return n.blindInsert(prefix, k1, k2, data)
+}
 
 func (n SimpleTrie) Update(key string, data interface{}) (Tree, bool) { panic("not implemented") }
 
@@ -19,24 +37,18 @@ func (n SimpleTrie) Delete(key string) (interface{}, Tree, bool) { panic("not im
 
 func (n SimpleTrie) Query(key string) (interface{}, bool) { panic("not implemented") }
 
-func (n SimpleTrie) isUninit() bool { return n.key == "" && n.data == nil && n.sons == nil }
-func (n SimpleTrie) isLeaf() bool   { return n.key != "" && n.sons == nil }
+func (n SimpleTrie) isUninit() bool   { return n.key == "" && n.data == nil && n.sons == nil }
+func (n SimpleTrie) isLeaf() bool     { return n.key != "" && n.sons == nil }
+func (n SimpleTrie) isWildcard() bool { return n.key == "" && n.sons != nil }
 
-func (n SimpleTrie) insert(key string, data interface{}) (SimpleTrie, bool) {
-	if n.isUninit() {
-		return SimpleTrie{key: key, data: data}, true
-	}
-	prefix, k1, k2 := lcs(n.key, key)
-	if prefix == "" { // No common prefix
-		return SimpleTrie{sons: []SimpleTrie{n, {key: key, data: data}}}, true
-	}
+func (n SimpleTrie) blindInsert(prefix, k1, k2 string, data interface{}) (SimpleTrie, bool) {
 	if k1 == "" {
 		if k2 == "" { // Same object
 			return n, false
 		}
-		for i := 0; i < len(n.sons); i++ {
-			if prefix, _, _ := lcs(n.sons[i].key, k2); prefix != "" {
-				ns, ok := n.sons[i].insert(k2, data)
+		for i, s := range n.sons {
+			if prefix, k1, k2 := lcs(s.key, k2); prefix != "" {
+				ns, ok := s.blindInsert(prefix, k1, k2, data)
 				if ok {
 					n.sons[i] = ns
 				}
@@ -44,12 +56,15 @@ func (n SimpleTrie) insert(key string, data interface{}) (SimpleTrie, bool) {
 			}
 		}
 		return SimpleTrie{
-			key: n.key, data: n.data,
-			sons: append(n.sons, SimpleTrie{key: k2, data: data}),
+			key: n.key, data: n.data, word: n.word,
+			sons: append(n.sons, SimpleTrie{key: k2, data: data, word: true}),
 		}, true
 	}
-	n1 := SimpleTrie{key: k1, data: n.data, sons: n.sons}
-	n2 := SimpleTrie{key: k2, data: data}
+	n1 := SimpleTrie{key: k1, data: n.data, word: n.word, sons: n.sons}
+	if k2 == "" {
+		return SimpleTrie{key: prefix, sons: []SimpleTrie{n1}, word: true}, true
+	}
+	n2 := SimpleTrie{key: k2, data: data, word: true}
 	return SimpleTrie{key: prefix, sons: []SimpleTrie{n1, n2}}, true
 }
 
@@ -62,4 +77,18 @@ func (n SimpleTrie) String() string {
 		ss = append(ss, s.String())
 	}
 	return fmt.Sprintf("\"%s\": [%s]", n.key, strings.Join(ss, ","))
+}
+
+func (n SimpleTrie) Words() []string { return n.words("") }
+func (n SimpleTrie) words(prefix string) (res []string) {
+	prefix = fmt.Sprintf("%s%s", prefix, n.key)
+	if n.word {
+		res = append(res, prefix)
+	}
+	for _, s := range n.sons {
+		if sres := s.words(prefix); sres != nil {
+			res = append(res, sres...)
+		}
+	}
+	return
 }
--- simple_test.go
+++ simple_test.go
@@ -1,6 +1,7 @@
 package gotrie_test
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/ar3s3ru/gotrie"
@@ -18,19 +19,67 @@ func TestSimpleTrie_Insert(t *testing.T) {
 			keys: []string{"amore", "amare", "amici"},
 			repr: `"am": ["ore" -> data: <nil>,"are" -> data: <nil>,"ici" -> data: <nil>]`,
 		},
+		{
+			name: "2 loosely similar keys and 1 totally different",
+			keys: []string{"amore", "insieme", "a te"},
+			repr: `"": ["a": ["more" -> data: <nil>," te" -> data: <nil>],"insieme" -> data: <nil>]`,
+		},
+		{
+			name: "2 loosely similar keys and 2 totally different",
+			keys: []string{"amore", "insieme", "a", "te"},
+			repr: `"": ["a": ["more" -> data: <nil>],"insieme" -> data: <nil>,"te" -> data: <nil>]`,
+		},
 	}
 
-	for i, test := range testcases {
+	for _, test := range testcases {
 		t.Run(test.name, func(t *testing.T) {
 			var tt gotrie.SimpleTrie
 			for _, s := range test.keys {
 				nt, ok := tt.Insert(s, nil)
-				if !assert.True(t, ok, "test %d: not ok", i) {
+				if !assert.True(t, ok, "not ok") {
 					break
 				}
 				tt = nt.(gotrie.SimpleTrie)
 			}
-			assert.Equal(t, test.repr, tt.String(), "test %d: result mismatch", i)
+			assert.Equal(t, test.repr, tt.String(), "result mismatch")
+		})
+	}
+}
+
+func TestSimpleTrie_Words(t *testing.T) {
+	testcases := []struct {
+		name string
+		keys []string
+		repr string
+	}{
+		{
+			name: "Simple test with 3 similar keys",
+			keys: []string{"amore", "amare", "amici"},
+		},
+		{
+			name: "2 loosely similar keys and 1 totally different",
+			keys: []string{"amore", "insieme", "a te"},
+		},
+		{
+			name: "2 loosely similar keys and 2 totally different",
+			keys: []string{"amore", "insieme", "a", "te"},
+		},
+	}
+
+	for _, test := range testcases {
+		t.Run(test.name, func(t *testing.T) {
+			var tt gotrie.SimpleTrie
+			for _, s := range test.keys {
+				nt, ok := tt.Insert(s, nil)
+				if !assert.True(t, ok, "not ok") {
+					break
+				}
+				tt = nt.(gotrie.SimpleTrie)
+			}
+			words := tt.Words()
+			sort.Strings(test.keys)
+			sort.Strings(words)
+			assert.Equal(t, test.keys, words, "words mismatch")
 		})
 	}
 }
