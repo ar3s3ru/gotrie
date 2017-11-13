2040c854c51e1b62487d4fbc594a022d9978a6cc
Danilo Cianfrone <b.zurchiele80@gmail.com>
2017-11-13 20:45:37 +0100

    added derekparker/trie benchmark

---
+++ derekparker_trie_bench_test.go
@@ -0,0 +1,30 @@
+package gotrie_test
+
+import (
+	"testing"
+
+	"github.com/derekparker/trie"
+	"github.com/stretchr/testify/assert"
+)
+
+func BenchmarkDerekParkerTrie_Insert(b *testing.B) {
+	tt := trie.New()
+	for n := 0; n < b.N; n++ {
+		i := n % len(words)
+		node := tt.Add(words[i], nil)
+		assert.NotNil(b, node, "insertion failed [%s]: node: %v", words[i], node)
+	}
+}
+
+func BenchmarkDerekParkerTrie_InsertAll(b *testing.B) {
+	b.Log("Words length is ", len(words))
+	b.Run("", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			tt := trie.New()
+			for _, w := range words {
+				node := tt.Add(w, nil)
+				assert.NotNil(b, node, "insertion failed [%s]: node: %v", w, node)
+			}
+		}
+	})
+}
